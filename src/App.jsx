a2c4da72e49b03a4456d2d48c311eab694dda120
Om <omchimurkar10@gmail.com>
2025-03-18 12:36:52 +0530

    speed insights added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Main from './components/initial.jsx';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
