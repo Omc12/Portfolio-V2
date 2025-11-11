3b15ed1c93c53d15123157a14869cf80cb780d58
Om <omchimurkar10@gmail.com>
2025-11-11 10:36:19 +0530

    about updated

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -7,13 +7,13 @@ const AboutSection = () => {
       <h2 id="aboutMini">About</h2>
       <div id="aboutSub">
         <p id="aboutPara">
-          <AnimatedText text= 'I am currently active as a frontend developer. I specialize in UI development & styling. And above all, I love doing it.'/>
+          <AnimatedText text= 'I am currently working with Machine Learning and LLMs. I am learning about Transformers & Deep Learning. I am also a MERN Stack developer.'/>
         </p>
         <p id="aboutParaMini1">
-          I'm a student who thrives on exploring innovative frameworks and programming languages to continuously enhance my tech stack. Every day, I deepen my understanding of how these technologies integrate to create seamless and effective solutions.
+          I’ve explored techniques like Linear Regression, KNNs, XGBoost, RFR, basic neural networks, data preprocessing and model evaluation, applying them in small but meaningful projects such as house price prediction, image classification, and working with classic datasets like Iris.
         </p>
         <p id="aboutParaMini2">
-          I'm working on a variety of projects that let me explore these new technologies in a hands-on way. Along the way, I'm also diving into machine learning and applying what I learn through real-world projects.
+          I’ve been learning about deep learning and transformers by first strengthening my base in models like linear regression and basic neural networks, then exploring how attention mechanisms, sequence modeling, applying these concepts in small experiments.
         </p>
       </div>
     </div>
